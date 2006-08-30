4385dfdeeba59054d6e631e501cff3ff66ebfa14
Niklas Gustavsson <ngn@apache.org>
2006-08-30 10:04:45 +0000

    Adding tests for passive transfer
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ftpserver/trunk@438443 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/ftpserver/clienttests/RetrieveTest.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */  
+
+package org.apache.ftpserver.clienttests;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.ftpserver.test.TestUtil;
+import org.apache.ftpserver.util.IoUtils;
+
+
+public class RetrieveTest extends ClientTestTemplate {
+    private static final String TEST_FILENAME = "test.txt";
+    private static final String DEFAULT_UNIQUE_FILENAME = "ftp.dat";
+
+    private static byte[] testData = null;
+    
+    /* (non-Javadoc)
+     * @see org.apache.ftpserver.clienttests.ClientTestTemplate#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        testData = "TESTDATA".getBytes("UTF-8");
+        
+        client.login(ADMIN_USERNAME, ADMIN_PASSWORD);
+        client.setRemoteVerificationEnabled(false);
+        client.enterLocalPassiveMode();
+    }
+
+
+    private void writeDataToFile(File file, byte[] data) throws IOException {
+        FileOutputStream fos = null;
+        
+        try{
+            fos = new FileOutputStream(file);
+            
+            fos.write(data);
+        } finally {
+            IoUtils.close(fos);
+        }
+    }
+
+    public void testRetrieve() throws Exception {
+        File testFile = new File(rootDir, TEST_FILENAME);
+        
+        writeDataToFile(testFile, testData);
+
+        assertTrue(testFile.exists());
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        assertTrue(client.retrieveFile(TEST_FILENAME, baos));
+        
+        assertTrue(testFile.exists());
+        TestUtil.assertArraysEqual(testData, baos.toByteArray());
+    }
+
+    public void testRetrieveWithPath() throws Exception {
+        File dir = new File(rootDir, "foo/bar");
+        dir.mkdirs();
+        
+        File testFile = new File(dir, TEST_FILENAME);
+        
+        writeDataToFile(testFile, testData);
+        
+        assertTrue(testFile.exists());
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        assertTrue(client.retrieveFile("foo/bar/" + TEST_FILENAME, baos));
+        
+        assertTrue(testFile.exists());
+        TestUtil.assertArraysEqual(testData, baos.toByteArray());
+    }
+
+    public void testRetrieveNonExistingFile() throws Exception {
+        File testFile = new File(rootDir, TEST_FILENAME);
+        
+        assertFalse(testFile.exists());
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        assertFalse(client.retrieveFile(TEST_FILENAME, baos));
+    }
+}
