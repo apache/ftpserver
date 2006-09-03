4a1ef93f2d3d93d5442dbb3cc6ed384c2f364dfd
Niklas Gustavsson <ngn@apache.org>
2006-09-03 13:12:26 +0000

    Replaced MD5 hashing with doing it over a stream. Adding MD5 to the supported features in FEAT
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ftpserver/trunk@439773 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/ftpserver/command/MD5.java
+++ src/java/org/apache/ftpserver/command/MD5.java
@@ -19,11 +19,12 @@
 
 package org.apache.ftpserver.command;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.ftpserver.FtpRequestImpl;
 import org.apache.ftpserver.FtpWriter;
 import org.apache.ftpserver.RequestHandler;
@@ -94,21 +95,10 @@ class MD5 implements ICommand {
             }
             
             InputStream is = null;
-            ByteArrayOutputStream baos = null;
             try{
                 is = file.createInputStream(0);
-                baos = new ByteArrayOutputStream();
-                
-                IoUtils.copy(is, baos, 1024);
-                
-                // try to close before returning command, 
-                // potentital race condition otherwise 
-                IoUtils.close(is);
-                IoUtils.close(baos);
-                
-                String md5Hash = DigestUtils.md5Hex(baos.toByteArray());
-                md5Hash = md5Hash.toUpperCase();
-    
+                String md5Hash = md5(is);
+
                 if(i > 0) {
                     sb.append(", ");
                 }
@@ -117,11 +107,10 @@ class MD5 implements ICommand {
                 sb.append(' ');
                 sb.append(md5Hash);
                 
-                
+            } catch(NoSuchAlgorithmException e) {
+                out.send(502, "MD5.notimplemened", null);
             } finally {
-                // Just to make sure
                 IoUtils.close(is);
-                IoUtils.close(baos);
             }
         }
         if(isMMD5) {
@@ -129,6 +118,57 @@ class MD5 implements ICommand {
         } else {
             out.send(251, "MD5", sb.toString());
         }
-        return;
-    }   
+    }
+
+    /**
+     * @param is InputStream for which the MD5 hash is calculated
+     * @return
+     * @throws IOException
+     * @throws NoSuchAlgorithmException 
+     */
+    private String md5(InputStream is) throws IOException, NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        DigestInputStream dis = new DigestInputStream(is, digest);
+
+        byte[] buffer = new byte[1024];
+
+        int read = dis.read(buffer);
+        while (read > -1) {
+            read = dis.read(buffer);
+        }
+
+        return new String(encodeHex(dis.getMessageDigest().digest()));
+    } 
+    
+    /**
+     * Converts an array of bytes into an array of characters representing the
+     * hexidecimal values of each byte in order. The returned array will be
+     * double the length of the passed array, as it takes two characters to
+     * represent any given byte.
+     * 
+     * @param data
+     *            a byte[] to convert to Hex characters
+     * @return A char[] containing hexidecimal characters
+     */
+    public static char[] encodeHex(byte[] data) {
+
+        int l = data.length;
+
+        char[] out = new char[l << 1];
+
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = DIGITS[(0xF0 & data[i]) >>> 4];
+            out[j++] = DIGITS[0x0F & data[i]];
+        }
+
+        return out;
+    }
+
+    /**
+     * Used to build output as Hex
+     */
+    private static final char[] DIGITS = {'0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
 }
