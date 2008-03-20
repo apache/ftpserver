bd5cd12515141193278ebcb88425875613942d5e
Niklas Gustavsson <ngn@apache.org>
2008-03-20 20:56:22 +0000

    Removing the old Connection related classes, their behavior is now handled by MINA (like idle control).
    git-svn-id: https://svn.apache.org/repos/asf/mina/ftpserver/trunk@639456 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/ftpserver/DefaultConnectionConfig.java
+++ core/src/main/java/org/apache/ftpserver/DefaultConnectionConfig.java
@@ -1,30 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */  
+
 package org.apache.ftpserver;
 
 public class DefaultConnectionConfig implements ConnectionConfig {
 
+    private int maxLogins         = 10;
+    private boolean anonymousLoginEnabled       = true;
+    private int maxAnonymousLogins     = 10;
+    private int maxLoginFailures  = 3;
+    private int loginFailureDelay = 500;
+	
 	public int getLoginFailureDelay() {
-		// TODO Auto-generated method stub
-		return 0;
+		return loginFailureDelay;
 	}
 
 	public int getMaxAnonymousLogins() {
-		// TODO Auto-generated method stub
-		return 0;
+		return maxAnonymousLogins;
 	}
 
 	public int getMaxLoginFailures() {
-		// TODO Auto-generated method stub
-		return 0;
+		return maxLoginFailures;
 	}
 
 	public int getMaxLogins() {
-		// TODO Auto-generated method stub
-		return 0;
+		return maxLogins;
 	}
 
 	public boolean isAnonymousLoginEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return anonymousLoginEnabled;
+	}
+
+	public void setMaxLogins(int maxLogins) {
+		this.maxLogins = maxLogins;
+	}
+
+	public void setAnonymousLoginEnabled(boolean anonymousLoginEnabled) {
+		this.anonymousLoginEnabled = anonymousLoginEnabled;
+	}
+
+	public void setMaxAnonymousLogins(int maxAnonymousLogins) {
+		this.maxAnonymousLogins = maxAnonymousLogins;
+	}
+
+	public void setMaxLoginFailures(int maxLoginFailures) {
+		this.maxLoginFailures = maxLoginFailures;
+	}
+
+	public void setLoginFailureDelay(int loginFailureDelay) {
+		this.loginFailureDelay = loginFailureDelay;
 	}
 
 }
