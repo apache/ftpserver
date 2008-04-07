fa7f0343a961a4599c5d947d4b6334b356d90861
Niklas Gustavsson <ngn@apache.org>
2008-04-07 20:47:48 +0000

    + New methods to keep track with MINA additions Improved JavaDoc
    git-svn-id: https://svn.apache.org/repos/asf/mina/ftpserver/trunk@645678 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/ftpserver/interfaces/FtpIoSession.java
+++ core/src/main/java/org/apache/ftpserver/interfaces/FtpIoSession.java
@@ -59,244 +59,424 @@ public class FtpIoSession implements IoSession {
 	
     /* Begin wrapped IoSession methods */
     
+	/**
+	 * @see IoSession#close()
+	 */
 	public CloseFuture close() {
 		return wrappedSession.close();
 	}
 
+	/**
+	 * @see IoSession#close(boolean)
+	 */
 	public CloseFuture close(boolean immediately) {
 		return wrappedSession.close(immediately);
 	}
 
+	/**
+	 * @see IoSession#closeOnFlush()
+	 */
 	public CloseFuture closeOnFlush() {
 		return wrappedSession.closeOnFlush();
 	}
 
+	/**
+	 * @see IoSession#containsAttribute(Object)
+	 */
 	public boolean containsAttribute(Object key) {
 		return wrappedSession.containsAttribute(key);
 	}
 
+	/**
+	 * @see IoSession#getAttachment()
+	 */
 	@SuppressWarnings("deprecation")
 	public Object getAttachment() {
 		return wrappedSession.getAttachment();
 	}
 
+	/**
+	 * @see IoSession#getAttribute(Object)
+	 */
 	public Object getAttribute(Object key) {
 		return wrappedSession.getAttribute(key);
 	}
 
+	/**
+	 * @see IoSession#getAttribute(Object, Object)
+	 */
 	public Object getAttribute(Object key, Object defaultValue) {
 		return wrappedSession.getAttribute(key, defaultValue);
 	}
 
+	/**
+	 * @see IoSession#getAttributeKeys()
+	 */
 	public Set<Object> getAttributeKeys() {
 		return wrappedSession.getAttributeKeys();
 	}
 
+	/**
+	 * @see IoSession#getBothIdleCount()
+	 */
 	public int getBothIdleCount() {
 		return wrappedSession.getBothIdleCount();
 	}
 
+	/**
+	 * @see IoSession#getCloseFuture()
+	 */
 	public CloseFuture getCloseFuture() {
 		return wrappedSession.getCloseFuture();
 	}
 
+	/**
+	 * @see IoSession#getConfig()
+	 */
 	public IoSessionConfig getConfig() {
 		return wrappedSession.getConfig();
 	}
 
+	/**
+	 * @see IoSession#getCreationTime()
+	 */
 	public long getCreationTime() {
 		return wrappedSession.getCreationTime();
 	}
 
+	/**
+	 * @see IoSession#getFilterChain()
+	 */
 	public IoFilterChain getFilterChain() {
 		return wrappedSession.getFilterChain();
 	}
 
+	/**
+	 * @see IoSession#getHandler()
+	 */
 	public IoHandler getHandler() {
 		return wrappedSession.getHandler();
 	}
 
+	/**
+	 * @see IoSession#getId()
+	 */
 	public long getId() {
 		return wrappedSession.getId();
 	}
 
+	/**
+	 * @see IoSession#getIdleCount(IdleStatus)
+	 */
 	public int getIdleCount(IdleStatus status) {
 		return wrappedSession.getIdleCount(status);
 	}
 
+	/**
+	 * @see IoSession#getLastBothIdleTime()
+	 */
 	public long getLastBothIdleTime() {
 		return wrappedSession.getLastBothIdleTime();
 	}
 
+	/**
+	 * @see IoSession#getLastIdleTime(IdleStatus)
+	 */
 	public long getLastIdleTime(IdleStatus status) {
 		return wrappedSession.getLastIdleTime(status);
 	}
 
+	/**
+	 * @see IoSession#getLastIoTime()
+	 */
 	public long getLastIoTime() {
 		return wrappedSession.getLastIoTime();
 	}
 
+	/**
+	 * @see IoSession#getLastReadTime()
+	 */
 	public long getLastReadTime() {
 		return wrappedSession.getLastReadTime();
 	}
 
+	/**
+	 * @see IoSession#getLastReaderIdleTime()
+	 */
 	public long getLastReaderIdleTime() {
 		return wrappedSession.getLastReaderIdleTime();
 	}
 
+	/**
+	 * @see IoSession#getLastWriteTime()
+	 */
 	public long getLastWriteTime() {
 		return wrappedSession.getLastWriteTime();
 	}
 
+	/**
+	 * @see IoSession#getLastWriterIdleTime()
+	 */
 	public long getLastWriterIdleTime() {
 		return wrappedSession.getLastWriterIdleTime();
 	}
 
+	/**
+	 * @see IoSession#getLocalAddress()
+	 */
 	public SocketAddress getLocalAddress() {
 		return wrappedSession.getLocalAddress();
 	}
 
+	/**
+	 * @see IoSession#getReadBytes()
+	 */
 	public long getReadBytes() {
 		return wrappedSession.getReadBytes();
 	}
 
+	/**
+	 * @see IoSession#getReadBytesThroughput()
+	 */
 	public double getReadBytesThroughput() {
 		return wrappedSession.getReadBytesThroughput();
 	}
 
+	/**
+	 * @see IoSession#getReadMessages()
+	 */
 	public long getReadMessages() {
 		return wrappedSession.getReadMessages();
 	}
 
+	/**
+	 * @see IoSession#getReadMessagesThroughput()
+	 */
 	public double getReadMessagesThroughput() {
 		return wrappedSession.getReadMessagesThroughput();
 	}
 
+	/**
+	 * @see IoSession#getReaderIdleCount()
+	 */
 	public int getReaderIdleCount() {
 		return wrappedSession.getReaderIdleCount();
 	}
 
+	/**
+	 * @see IoSession#getRemoteAddress()
+	 */
 	public SocketAddress getRemoteAddress() {
 		return wrappedSession.getRemoteAddress();
 	}
 
+	/**
+	 * @see IoSession#getScheduledWriteBytes()
+	 */
 	public long getScheduledWriteBytes() {
 		return wrappedSession.getScheduledWriteBytes();
 	}
 
+	/**
+	 * @see IoSession#getScheduledWriteMessages()
+	 */
 	public int getScheduledWriteMessages() {
 		return wrappedSession.getScheduledWriteMessages();
 	}
 
+	/**
+	 * @see IoSession#getService()
+	 */
 	public IoService getService() {
 		return wrappedSession.getService();
 	}
 
+	/**
+	 * @see IoSession#getServiceAddress()
+	 */
 	public SocketAddress getServiceAddress() {
 		return wrappedSession.getServiceAddress();
 	}
 
+	/**
+	 * @see IoSession#getTrafficMask()
+	 */
 	public TrafficMask getTrafficMask() {
 		return wrappedSession.getTrafficMask();
 	}
 
+	/**
+	 * @see IoSession#getTransportMetadata()
+	 */
 	public TransportMetadata getTransportMetadata() {
 		return wrappedSession.getTransportMetadata();
 	}
 
+	/**
+	 * @see IoSession#getWriterIdleCount()
+	 */
 	public int getWriterIdleCount() {
 		return wrappedSession.getWriterIdleCount();
 	}
 
+	/**
+	 * @see IoSession#getWrittenBytes()
+	 */
 	public long getWrittenBytes() {
 		return wrappedSession.getWrittenBytes();
 	}
 
+	/**
+	 * @see IoSession#getWrittenBytesThroughput()
+	 */
 	public double getWrittenBytesThroughput() {
 		return wrappedSession.getWrittenBytesThroughput();
 	}
 
+	/**
+	 * @see IoSession#getWrittenMessages()
+	 */
 	public long getWrittenMessages() {
 		return wrappedSession.getWrittenMessages();
 	}
 
+	/**
+	 * @see IoSession#getWrittenMessagesThroughput()
+	 */
 	public double getWrittenMessagesThroughput() {
 		return wrappedSession.getWrittenMessagesThroughput();
 	}
 
+	/**
+	 * @see IoSession#isClosing()
+	 */
 	public boolean isClosing() {
 		return wrappedSession.isClosing();
 	}
 
+	/**
+	 * @see IoSession#isConnected()
+	 */
 	public boolean isConnected() {
 		return wrappedSession.isConnected();
 	}
 
+	/**
+	 * @see IoSession#isIdle(IdleStatus)
+	 */
 	public boolean isIdle(IdleStatus status) {
 		return wrappedSession.isIdle(status);
 	}
 
+	/**
+	 * @see IoSession#read()
+	 */
 	public ReadFuture read() {
 		return wrappedSession.read();
 	}
 
+	/**
+	 * @see IoSession#removeAttribute(Object)
+	 */
 	public Object removeAttribute(Object key) {
 		return wrappedSession.removeAttribute(key);
 	}
 
+	/**
+	 * @see IoSession#removeAttribute(Object, Object)
+	 */
 	public boolean removeAttribute(Object key, Object value) {
 		return wrappedSession.removeAttribute(key, value);
 	}
 
+	/**
+	 * @see IoSession#replaceAttribute(Object, Object, Object)
+	 */
 	public boolean replaceAttribute(Object key, Object oldValue, Object newValue) {
 		return wrappedSession.replaceAttribute(key, oldValue, newValue);
 	}
 
+	/**
+	 * @see IoSession#resumeRead()
+	 */
 	public void resumeRead() {
 		wrappedSession.resumeRead();
 	}
 
+	/**
+	 * @see IoSession#resumeWrite()
+	 */
 	public void resumeWrite() {
 		wrappedSession.resumeWrite();
 	}
 
+	/**
+	 * @see IoSession#setAttachment(Object)
+	 */
 	@SuppressWarnings("deprecation")
 	public Object setAttachment(Object attachment) {
 		return wrappedSession.setAttachment(attachment);
 	}
 
+	/**
+	 * @see IoSession#setAttribute(Object)
+	 */
 	public Object setAttribute(Object key) {
 		return wrappedSession.setAttribute(key);
 	}
 
+	/**
+	 * @see IoSession#setAttribute(Object, Object)
+	 */
 	public Object setAttribute(Object key, Object value) {
 		return wrappedSession.setAttribute(key, value);
 	}
 
+	/**
+	 * @see IoSession#setAttributeIfAbsent(Object)
+	 */
 	public Object setAttributeIfAbsent(Object key) {
 		return wrappedSession.setAttributeIfAbsent(key);
 	}
 
+	/**
+	 * @see IoSession#setAttributeIfAbsent(Object, Object)
+	 */
 	public Object setAttributeIfAbsent(Object key, Object value) {
 		return wrappedSession.setAttributeIfAbsent(key, value);
 	}
 
+	/**
+	 * @see IoSession#setTrafficMask(TrafficMask)
+	 */
 	public void setTrafficMask(TrafficMask trafficMask) {
 		wrappedSession.setTrafficMask(trafficMask);
 	}
 
+	/**
+	 * @see IoSession#suspendRead()
+	 */
 	public void suspendRead() {
 		wrappedSession.suspendRead();
 	}
 
+	/**
+	 * @see IoSession#suspendWrite()
+	 */
 	public void suspendWrite() {
 		wrappedSession.suspendWrite();
 	}
 
+	/**
+	 * @see IoSession#write(Object)
+	 */
 	public WriteFuture write(Object message) {
 		return wrappedSession.write(message);
 	}
 
+	/**
+	 * @see IoSession#write(Object, SocketAddress)
+	 */
 	public WriteFuture write(Object message, SocketAddress destination) {
 		return wrappedSession.write(message, destination);
 	}
@@ -477,12 +657,39 @@ public class FtpIoSession implements IoSession {
 		
 	}
 
+	/**
+	 * @see IoSession#getCurrentWriteMessage()
+	 */
 	public Object getCurrentWriteMessage() {
 		return wrappedSession.getCurrentWriteMessage();
 	}
 
+	/**
+	 * @see IoSession#getCurrentWriteRequest()
+	 */
 	public WriteRequest getCurrentWriteRequest() {
 		return wrappedSession.getCurrentWriteRequest();
+	}
+
+	/**
+	 * @see IoSession#isBothIdle()
+	 */
+	public boolean isBothIdle() {
+		return wrappedSession.isBothIdle();
+	}
+
+	/**
+	 * @see IoSession#isReaderIdle()
+	 */
+	public boolean isReaderIdle() {
+		return wrappedSession.isReaderIdle();
+	}
+
+	/**
+	 * @see IoSession#isWriterIdle()
+	 */
+	public boolean isWriterIdle() {
+		return wrappedSession.isWriterIdle();
 	}
 
 }
