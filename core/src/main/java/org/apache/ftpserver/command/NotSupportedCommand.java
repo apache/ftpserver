89529d22d0b3647583dface640ca8498dd9f7b22
Niklas Gustavsson <ngn@apache.org>
2008-05-15 12:17:01 +0000

    Adding command used for disabling (by overriding) default commands.
    git-svn-id: https://svn.apache.org/repos/asf/mina/ftpserver/trunk@656619 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/ftpserver/command/NotSupportedCommand.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */  
+
+package org.apache.ftpserver.command;
+
+import java.io.IOException;
+
+import org.apache.ftpserver.ftplet.FtpReply;
+import org.apache.ftpserver.ftplet.FtpRequest;
+import org.apache.ftpserver.interfaces.FtpIoSession;
+import org.apache.ftpserver.interfaces.FtpServerContext;
+import org.apache.ftpserver.util.FtpReplyUtil;
+
+/**
+ * A command used primarily for overriding already
+ * installed commands when one wants to disable the command.
+ */
+public 
+class NotSupportedCommand extends AbstractCommand {
+
+    /**
+     * Execute command
+     */
+    public void execute(FtpIoSession session, 
+    		FtpServerContext context,
+            FtpRequest request) throws IOException {
+        
+        // reset state variables
+        session.resetState();
+        
+        // We do not support this command
+        session.write(FtpReplyUtil.translate(session, request, context, FtpReply.REPLY_502_COMMAND_NOT_IMPLEMENTED, "Not supported", null));
+    }   
+}
