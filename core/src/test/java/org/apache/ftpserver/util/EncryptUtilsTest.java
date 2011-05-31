3cb3f6bdd71667fa16d3c96bf08de46ea00a2dc7
Sebastian Bazley <sebb@apache.org>
2011-05-31 15:09:34 +0000

    Unnecessary throws clause
    git-svn-id: https://svn.apache.org/repos/asf/mina/ftpserver/trunk@1129738 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/ftpserver/util/EncryptUtilsTest.java
+++ core/src/test/java/org/apache/ftpserver/util/EncryptUtilsTest.java
@@ -21,8 +21,6 @@ package org.apache.ftpserver.util;
 
 import junit.framework.TestCase;
 
-import org.apache.ftpserver.ftplet.FtpException;
-
 /**
 *
 * @author <a href="http://mina.apache.org">Apache MINA Project</a>
@@ -30,12 +28,12 @@ import org.apache.ftpserver.ftplet.FtpException;
 */
 public class EncryptUtilsTest extends TestCase {
 
-    public void testEncryptMd5() throws FtpException {
+    public void testEncryptMd5() {
         assertEquals("21232F297A57A5A743894A0E4A801FC3", EncryptUtils
                 .encryptMD5("admin"));
     }
 
-    public void testEncryptSha() throws FtpException {
+    public void testEncryptSha() {
         assertEquals("D033E22AE348AEB5660FC2140AEC35850C4DA997", EncryptUtils
                 .encryptSHA("admin"));
     }
